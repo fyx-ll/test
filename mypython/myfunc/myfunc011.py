4eeff0ea35a597d5f0f45ee4075067ced90cc24d
fyx-ll <fanwei0714@163.com>
2019-04-14 09:18:04 +0800

    201904140917update

--- mypython/myfunc/myfunc011.py
+++ mypython/myfunc/myfunc011.py
@@ -17,3 +17,17 @@ print(t)
 print(type(t))
 print(t[0](2), t[1](3), t[2](4))
 
+
+'''测试eval()函数：动态执行函数'''
+print('=====eval分割线======')
+s = "print('abcd')"
+eval(s)
+
+a = 10
+b = 20
+c = eval("a+b")
+print(c)
+
+dict1 = dict(a=100, b=200)
+print(eval("a+b")) #默认使用全局global
+print(eval("a+b", dict1)) #指定使用local()
