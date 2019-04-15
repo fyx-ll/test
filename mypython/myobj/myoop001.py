4a4399143c6990f8ee21329a62255e6d4328234c
fyx-ll <fanwei0714@163.com>
2019-04-15 23:44:24 +0800

    201904152344update

---
+++ mypython/myobj/myoop001.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''定义学生类'''
+
+
+class Student:  # 类名一般首字母大写，多个单词采用驼峰原则
+    #python中属性必须使用构造方法"__int__"定义，self代表当前对象本身
+    def __init__(self, name, score):              # self 必须位于第一个参数单词是 init 不是int
+        self.name = name
+        self.score = score
+
+    def say_score(self):  # self 必须是位于第一个参数。
+        print('{0}的分数是：{1}'.format(self.name, self.score))
+
+print(type(Student))
+print(id(Student))
+s1 = Student('fanwei', 98)
+print(type(s1))
+print(id(s1))
+
+s1.say_score()
