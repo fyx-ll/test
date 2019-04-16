d41a50b4252f0074f1239994bc1d12616db6a056
fyx-ll <fanwei0714@163.com>
2019-04-16 23:30:21 +0800

    201904162330update

--- mypython/myobj/myoop001.py
+++ mypython/myobj/myoop001.py
@@ -11,10 +11,19 @@ class Student:  # 类名一般首字母大写，多个单词采用驼峰原则
     def say_score(self):  # self 必须是位于第一个参数。
         print('{0}的分数是：{1}'.format(self.name, self.score))
 
-print(type(Student))
+print(type(Student)) #类型对象，也可以理解为模具
 print(id(Student))
 s1 = Student('fanwei', 98)
-print(type(s1))
-print(id(s1))
+print('s1类型', type(s1))
+print('s1的id', id(s1))
 
 s1.say_score()
+s1.score = 90
+s1.age = 35
+print(s1.age) #新加的属性，在s2中是没有的
+print(s1.score) #修改的属性
+
+s2 = Student('abc', 100)
+print('s2类型', type(s2))
+print('s2的id', id(s2))
+print(s2.score)
